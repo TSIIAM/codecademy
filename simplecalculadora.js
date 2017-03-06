cb0cd440e80a9b6688ab7848c983101244aca0e1
Alvaromesa <tsi.iamesa@gmail.com>
2017-03-06 20:51:15 -0300

    Update simplecalculadora.js

--- simplecalculadora.js
+++ simplecalculadora.js
@@ -11,7 +11,7 @@ multiInput.addEventListener('click', multi);
 var dividirInput = document.querySelector('#dividir');
 dividirInput.addEventListener('click', dividir);
 
-document.onreadystatechange = function (){
+$( document ).ready(function(){
                 function suma(){
                     
         //obtener valores
